a63292402bc5d7ed115a84449b3f7ef0b11c21e4
sandevadiga <santhoshrdevadiga@gmail.com>
2024-04-23 13:15:36 +0530

    resolve typo

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Head from "next/head";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navBar";
 import "./globals.css"; 
 
 export const metadata = {
