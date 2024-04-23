7e3a86a672a5d480ac3a527b3f5116a0376f1f27
sandevadiga <santhoshrdevadiga@gmail.com>
2024-04-23 12:56:15 +0530

    added

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,5 @@
-const path = require('path');
-
+User
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Add your custom webpack configurations here
-  webpack: (config, { isServer }) => {
-    // Add aliasing for imports
-    config.resolve.alias['@components'] = path.join(__dirname, 'components');
-    config.resolve.alias['@data'] = path.join(__dirname, 'data');
-    // Add more aliases as needed
+const nextConfig = {};
 
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+export default nextConfig;
